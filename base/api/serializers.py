2fc221a7e046f1a9ba0fafa27f2ba1cc08387115
kundan <987kundanrai@gmail.com>
2021-12-28 13:49:20 +0530

    added api feature in this prject

---
+++ base/api/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework.serializers import ModelSerializer
+from base.models import Room
+
+class RoomsSerializer(ModelSerializer):
+    class Meta:
+        model=Room
+        fields='__all__'
